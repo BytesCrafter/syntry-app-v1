4bd8e4cd5133dbb7eaa87fb960be6902578ccb68
Bytes Crafter <bytescrafter@gmail.com>
2021-05-10 17:43:24 +0800

    Added the correct firebase config for erpat ahm.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: 'https://beta.erpat.net/v1/api/',
+  baseURL: 'https://erpat.ahmoutsourcing.com/v1/api/',
   authToken: '9bc12f10eb077de'
 };
