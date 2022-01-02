471cc9f75936d3a6b763b84581594966851300ba
BytesCrafter <bytescrafter@gmail.com>
2022-01-03 01:30:19 +0800

    Added beta release of electron desktop app auto update.

--- main.js
+++ main.js
@@ -1,7 +1,82 @@
 const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater")
+const log = require('electron-log')
 const path = require('path')
 
-function createWindow () {
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+function splashWindow() {
+  win = new BrowserWindow({
+    width: 320,
+    height: 320,
+    resizable: false,
+    frame: false,
+    closable: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/splash.html#v${app.getVersion()}`);
+  return win;
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+  BrowserWindow.getAllWindows().forEach(function (win) {
+    win.close()
+  })
+  primaryWindow();
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+  autoUpdater.quitAndInstall(true, true);
+});
+
+function primaryWindow () {
   const win = new BrowserWindow({
     width: 480,
     height: 720,
@@ -22,15 +97,13 @@ function createWindow () {
   //win.webContents.openDevTools()
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//-------------------------------------------------------------------
+app.on('ready', function()  {
+  splashWindow();
+  autoUpdater.checkForUpdates(); //checkForUpdatesAndNotify
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
