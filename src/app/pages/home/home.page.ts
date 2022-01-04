4eecf97f58611c48674361d5866765fc4e3e027d
BytesCrafter <bytescrafter@gmail.com>
2022-01-04 14:09:02 +0800

    Added Sigin and signout on homepage of the app.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
+import * as moment from 'moment';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-home',
@@ -14,17 +16,32 @@ export class HomePage implements OnInit {
 
   constructor(
     private api: ApiService,
-    public auth: AuthService
+    public auth: AuthService,
+    private util: UtilService
   ) {
+    this.getMyClockedList();
+  }
+
+  getMyClockedList() {
     this.api.get('attendance/my_clocked_in_list').subscribe((response: any) => {
       if(response.success) {
-        this.previous = null;
+        this.serverDatetime = new Date(response.status.local_time);
+        setInterval(() => {
+          this.serverDatetime.setSeconds(this.serverDatetime.getSeconds() + 1);
+        }, 1000);
+        if(response.status.clocked_in) {
+          this.clockedinDatetime = new Date(response.status.clocked_in);
+        } else {
+          this.clockedinDatetime = null;
+        }
 
+        this.previous = null;
+        this.attendance = [];
         const clockedIn: any[] = response.data;
         clockedIn.forEach(attd => {
           this.attendance.push(
             {
-              avatar: auth.userToken.avatar,
+              id: attd.id,
               timein: attd.in_time,
               timeout: attd.out_time,
               duration: attd.duration
@@ -36,7 +53,59 @@ export class HomePage implements OnInit {
     });
   }
 
+  public get isClockedIn(): any {
+    return this.clockedinDatetime ? true:false;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  private serverDatetime: Date = new Date();
+  public get currentDate(): any {
+    return this.serverDatetime.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  }
+  public get currentTime(): any {
+    return this.serverDatetime.toLocaleTimeString();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  private clockedinDatetime: Date = new Date();
+  public get currentDatestart(): any {
+    return 'Started'+moment(this.clockedinDatetime).format(' on h:mm:ss A of Do MMMM YYYY');
+  }
+  public get currentTimespan(): any {
+    const start = moment(this.clockedinDatetime);
+    const end = moment(this.serverDatetime);
+    const duration = moment.duration(end.diff(start));
+    // eslint-disable-next-line max-len
+    return Math.floor(duration.asHours())+'h '
+      +Math.floor(duration.asMinutes() % 60)+'m '
+      +Math.floor(duration.asSeconds() % 60)+'s';
+  }
+
   ngOnInit() {
   }
 
+  logTime() {
+    const userId = this.auth.userToken.id;
+    this.api.post('attendance/logtime/'+userId, {
+      self: this.auth.userToken.uuid
+    }).subscribe(async (res: any) => {
+
+      if(res.success === false) {
+        this.util.modalAlert('Action not Allowed', res.message);
+        await this.sleep(3000);
+        return;
+      }
+
+      this.util.playAudio();
+      const premsg = res.clocked ? 'Goodbye! ' : 'Welcome! ';
+      this.util.modalAlert(premsg, res.stamp, res.data.fname +' '+ res.data.lname);
+
+      this.getMyClockedList();
+      await this.sleep(3000);
+    });
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
