5963377abb0670beabd6582c64a31263913d93d0
BytesCrafter <bytescrafter@gmail.com>
2022-05-30 18:10:04 +0800

    Added server last update for currently loggedin user.

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { ComponentFactoryResolver, Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ApiService } from '../services/api.service';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -9,7 +10,23 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private router: Router,
-  ) { }
+    private api: ApiService
+  ) {
+
+    const uid = localStorage.getItem(AuthService.tokenKey);
+    if (uid && uid != null && uid !== 'null') {
+      this.api.posts('users/count_notification', {}).then((res: any) => {
+        console.log('main', res);
+      }).catch(error => {
+        console.log('error', error);
+      });
+      // setInterval(() => {
+      //   console.log('TODO: Get notification or server update.');
+      // }, 10000);
+    }
+
+  }
+
   canActivate(
       next: ActivatedRouteSnapshot,
       state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
